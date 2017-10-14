7e8b3e2622308fd3743d5a025ec7a2d4e5872052
J. Alexander Curtis <jalexandercurtis@gmail.com>
2017-10-14 17:16:02 -0600

    Accordions complete

---
+++ Accordions/Final/scripts.js
@@ -0,0 +1,16 @@
+var accordions = document.getElementsByClassName("accordion");
+
+for (var i = 0; i < accordions.length; i++) {
+  accordions[i].onclick = function() {
+    this.classList.toggle('is-open');
+
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      // accordion is currently open, so close it
+      content.style.maxHeight = null;
+    } else {
+      // accordion is currently closed, so open it
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  }
+}
